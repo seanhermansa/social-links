3b6e2bc05fa2afcb097431a25d493577124c128f
Seán Hermansa <fo.okyu@web.de>
2020-09-26 23:47:23 +0200

    Initial commit

---
+++ social-links.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Plugin Name: Social Links
+* Description: Adds social icons with links to profiles
+* Version: 1.0
+* Author: Seán Hermansa
+*
+**/
+
+// Exit if Accessed Directly
+if(!defined('ABSPATH')){
+	exit;
+}
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__) . '/includes/social-links-scripts.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__) . '/includes/social-links-class.php');
+
+// Register Widget
+function register_social_links(){
+	register_widget('Social_Links_Widget');
+}
+
+add_action('widgets_init', 'register_social_links');
